29b8c8c8cf407823a7e0c70b772585b54b5e713a
Nelson Scott <officialbadbeargames@gmail.com>
2016-02-15 15:22:11 -0500

    Put flocking and input together

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : Singleton<GameManager> 
 {
+	#region Fields
+	//Assigned in inspector
+	public GameObject beePrefab;
+	public GameObject player;
+
+	public GameObject obstacleObject;
+	#endregion
+
+
 	protected GameManager() {}
 
 	void Awake()
--- Assets/Scripts/Steering Behaviors/Enemy.cs
+++ Assets/Scripts/Steering Behaviors/Enemy.cs
@@ -3,5 +3,31 @@ using System.Collections;
 
 public class Enemy : Seeker
 {
-	
+	override public void Start()
+	{
+		seekerTarget = GameManager.Instance.player;
+
+		base.Start();
+	}
+
+	protected override void CalcSteeringForces ()
+	{
+		//ultimateForce += Separation(sepDistance) * seperationWeight;
+
+		//ultimateForce += Alignment(Flock.Direction) * alignmentWeight;
+
+		//ultimateForce += Cohesion(Flock.Centroid) * cohesionWeight;
+
+		ultimateForce += Arrival(seekerTarget.transform.position, arrivalSlowingDistance);
+
+		ultimateForce += Separation(safeDistance);
+
+		for (int i = GameManager.Instance.obstacleObject.transform.childCount - 1; i >= 0; i--)
+		{
+			AvoidObstacle(GameManager.Instance.obstacleObject.transform.GetChild(i).gameObject, safeDistance);
+		}
+		//ultimateForce += Seek(seekerTarget.transform.position);
+
+		base.CalcSteeringForces ();
+	}
 }
