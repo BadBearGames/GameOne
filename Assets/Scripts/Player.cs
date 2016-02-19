c104ca0c1aa89f74af7f713f90a5b49dc28c773e
Isaac Selleck <ixs5214@g.rit.edu>
2016-02-18 19:48:01 -0500

    Updated movement. Touch now works but still need to smooth it out and make it feel more natural

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour {
 
 	private NavMeshAgent navMeshAgent;
 	private bool moving;
+	public float speed;
 
 	// Use this for initialization
 	void Awake () {
@@ -13,7 +14,7 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+		/*
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
 
@@ -25,17 +26,23 @@ public class Player : MonoBehaviour {
 
 			}
 		}
-
+*/
 
 		if (Input.touchCount > 0) {
 			// The screen has been touched so store the touch
-			Touch touch = Input.GetTouch(0);
+			Touch touch = Input.GetTouch (0);
 
-			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+			if (touch.phase == TouchPhase.Stationary) {
 				// If the finger is on the screen, move the object smoothly to the touch position
-				Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 10));                
-				transform.position = Vector3.Lerp(transform.position, touchPosition, Time.deltaTime);
+
+				Vector3 touchPosition = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y, 10));  
+
+				speed = Time.deltaTime * touch.tapCount * 5;
+			
+				transform.position = Vector3.MoveTowards (transform.position, touchPosition, speed);
 			}
+		} else {
+			
 		}
 	}
 
